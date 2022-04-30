ab26e6111b0bd8aa73f77ce0c5958081a016cdd9
Gabriel Ayusso Guimaraes <gabriel@alienlab.com.br>
2022-04-30 02:23:56 -0300

    bugfix visao do dia

--- resources/views/components/managerWeeklyCard.blade.php
+++ resources/views/components/managerWeeklyCard.blade.php
@@ -2,15 +2,15 @@
 $bookingDate = DateTime::createFromFormat('Y-m-d H:i:s', $item->bookingDate);
 @endphp
 
-@if($booking->status != 'C')
+@if($item->bookingStatus != 'C')
 <span title="{{$item->serviceName}}" class="appointment badge {{status($item->bookingStatus)}}">
     <div href="/manager/booking/{{$item->bookingId}}/edit" style="position: relative;">
         <span>{{$bookingDate->format('H:i')}}</span> <span>{{$item->customerName}}</span><br>
         <small>R$ {{$item->servicePrice}}</small><br>
         <div class="mt-2">
-            <a href="{{route('booking.edit', $booking)}}"><i class="fas fa-search fa-lg"></i></a>
-            @if($booking->status === 'A')
-            <a href="{{route('manager.pay', $booking)}}" onclick="return confirm('Deseja marcar esse agendamento como pago?');" alt="Marcar como pago" style="position:absolute; right:0; bottom: 0;"><i class="fas fa-money-bill fa-2x"></i></a>
+            <a href="{{route('booking.edit', $item->bookingId)}}"><i class="fas fa-search fa-lg"></i></a>
+            @if($item->bookingStatus === 'A')
+            <a href="{{route('manager.pay', $item->bookingId)}}" onclick="return confirm('Deseja marcar esse agendamento como pago?');" alt="Marcar como pago" style="position:absolute; right:0; bottom: 0;"><i class="fas fa-money-bill fa-2x"></i></a>
             @endif
         </div>
     </div>
